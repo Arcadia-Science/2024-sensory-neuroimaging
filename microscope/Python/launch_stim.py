78c3d59bcbacffb9fc035fb52b5940f918e0470f
ilyakolb <ilya.kolb.0@gmail.com>
2024-05-21 15:03:13 -0700

    clean up and document

--- microscope/Python/launch_stim.py
+++ microscope/Python/launch_stim.py
@@ -1,30 +1,47 @@
-import serial as ser
+import argparse
 import time as tm
-import argparse, sys
 
-#define arduino port to connect to.
-arduino_name ='COM4'
-BUZZER_ON_TIME_S = 2
-BUZZER_OFF_TIME_S = 4
+import serial as ser
+
+arduino_name ='COM1' # arduino port (check in device manager)
+STIMULATOR_ON_TIME_S = 2 # seconds that the tactile stimulator is on per stimulation
+STIMULATOR_OFF_TIME_S = 4 # seconds that the tactile stimulator is off per stimulation
 
 if __name__ == '__main__':
+    '''
+    Controls the Teensy microcontroller to launch the tactile stimulator
+    and the LED light for a given duration.
+
+    Args:
+        --duration: int, overall duration of the recording in seconds
+        --light: str, color of the LED light to turn on. Can be 'BLUE', 'VIOLET', 'ALTERNATE',
+                or 'RESET'. If 'ALTERNATE', the blue and violet LEDs will alternate every camera frame.
+                If 'RESET', all LEDs and stimulator will be turned off.
+        --stim: bool, whether to launch the tactile stimulator
+
+    Example usage (CLI):
+        ```python launch_stim.py --duration 300 --light BLUE --stim```
+
+        * Turns on the blue LED light for 300 seconds
+        * Launches the tactile stimulator (2s on, 4s off, hardcoded above)
+    '''
     parser = argparse.ArgumentParser(
                     prog='Launch stim')
     parser.add_argument('--duration', type=int)
     parser.add_argument('--light', type=str)
-    parser.add_argument('--buzzer', action='store_true')
+    parser.add_argument('--stim', action='store_true')
     args = parser.parse_args()
 
     rec_duration_s = args.duration
     light = (args.light).upper()
-    buzzer = args.buzzer
+    stim = args.stim
 
     print(rec_duration_s)
     print(light)
-    print(f"buzzer: {buzzer}")
+    print(f"stim: {stim}")
 
     dev=ser.Serial(arduino_name)
-    if light == 'RESET':
+    if light == 'RESET': # turn everything off
         dev.write(b'SET ' + bytes('BLUE', 'UTF-8') +b'_STATUS 0;')
         tm.sleep(0.1)
         dev.write(b'SET ' + bytes('VIOLET', 'UTF-8') +b'_STATUS 0;')
@@ -36,16 +53,16 @@ if __name__ == '__main__':
         dev.write(b'SET ' + bytes(light, 'UTF-8') +b'_STATUS 1;')
         tm.sleep(5)
 
-        if buzzer:
-            print('Starting buzzer')
-            n_stims = rec_duration_s // (BUZZER_ON_TIME_S + BUZZER_OFF_TIME_S)
-            print(f'Running {n_stims} buzzer stimulations')
-            for i in range(n_stims):
+        if stim:
+            print('Starting tactile stimulator...')
+            n_stims = rec_duration_s // (STIMULATOR_ON_TIME_S + STIMULATOR_OFF_TIME_S)
+            print(f'Running {n_stims} stimulations')
+            for _i in range(n_stims):
                 dev.write(b'SET STIM_STATUS 1;')
-                tm.sleep(BUZZER_ON_TIME_S)
+                tm.sleep(STIMULATOR_ON_TIME_S)
                 dev.write(b'SET STIM_STATUS 0;')
-                tm.sleep(BUZZER_OFF_TIME_S)
+                tm.sleep(STIMULATOR_OFF_TIME_S)
 
         tm.sleep(0.1)
-        dev.write(b'SET ' + bytes(light, 'UTF-8') + b'_STATUS 0;')
+        dev.write(b'SET ' + bytes(light, 'UTF-8') + b'_STATUS 0;') # LED off
     dev.close()
